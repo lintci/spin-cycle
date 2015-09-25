523df6b4a90f505d75920af84ee01735627df9fa
Allen Madsen <blatyo@gmail.com>
2015-09-24 22:03:38 -0400

    Fix deploy.js

--- config/deploy.js
+++ config/deploy.js
@@ -1,13 +1,12 @@
 module.exports = {
   "production": {
-    "buildEnv": 'production'
+    "buildEnv": 'production',
     "store": {
       "type": "S3",
       "accessKeyId": process.env['ACCESS_KEY_ID'],
       "secretAccessKey": process.env['SECRET_ACCESS_KEY'],
       "bucket": "spincycle.lintci.com"
     },
-
     "assets": {
       "type": "s3",
       "accessKeyId": process.env['ACCESS_KEY_ID'],
